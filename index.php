eaf7112d88e872166220e7355098ca1cc2b326fd
Queniex <queniesalbiyah@gmail.com>
2022-09-25 10:25:41 +0700

    add template

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Students - DB</title>
+
+    <!-- Link daisyui -->
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@2.24.2/dist/full.css" rel="stylesheet" type="text/css" />
+
+    <!-- Link tailwind -->
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <script>
+        tailwind.config = {
+          theme: {
+            container: {
+              center: true,
+              padding: '16px'
+            },
+            extend: {
+              colors: {
+                primary: '#14b8a6',
+              },
+              screens: {
+                '2xl': '1320px',
+              }
+            }
+          }
+        }
+      </script>
+
+      <style type="text/tailwindcss">
+        /* import font */
+        @import url('https://fonts.googleapis.com/css2?family=Comic+Neue&family=Dosis:wght@500&family=Luckiest+Guy&family=Poppins:ital,wght@0,300;0,500;0,600;0,800;0,900;1,600&display=swap');
+
+        @layer utilities {
+         .hamburger-active > span:nth-child(1){
+           @apply rotate-45;
+         }
+
+         @layer base {
+          *{
+            font-family: 'Poppins', sans-serif;
+          }
+         }
+
+        /* *{
+          border: 1px solid blue
+         } */
+
+        }
+      </style>
+
+</head>
+<body>
+
+    <!-- Header Start -->
+    <header >
+      
+    </header>
+    <!-- Header End -->
+
+    <!-- Hero Section Start -->
+    <section >
+        
+    </section>
+    <!-- Hero Section End -->
+
+    <!-- Experience Section Start -->
+    <section >
+      
+    </section>
+    <!-- Skill Section End -->
+
+    <script src="dist/js/script.js"></script>
+    <!-- <script src="dist/js/jquery-3.6.1.min.js"></script>
+    <script>
+      $(document).ready(function(){
+            $('a.xd').click(function(e){
+              e.preventDefault()
+            })
+        })
+    </script> -->
+</body>
+</html>
